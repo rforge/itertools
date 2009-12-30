8a8062aaaa0e3b29849e95fbe17c9d54659341db
weston <weston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-30 02:54:02 +0000

    Slightly more description.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/itertools@4 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -35,23 +35,26 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 
 <h1>Welcome to the itertools project</h1>
 <p>
-The 'itertools' package provides a variety of functions used to
-create iterators.
+The 'itertools' package provides a variety of functions used to create iterators.
+It is strongly inspired by the Python itertools module, and includes a
+number of functions of the same name.  It is intended to be used with the
+'foreach' package, which has excellent support for iterators.
+Together, they simplify many programming tasks.
 </p>
 
 <h2>Functions included in itertools:</h2>
 <p>
 <ul>
-  <li>chain - chain multiple iterators together into one iterator</li>
-  <li>enumerate - create an enumeration from an iterator</li>
-  <li>ihasNext - add a hasNext method to an iterator</li>
-  <li>ifilter - only include values that evaluate true</li>
-  <li>ifilterfalse - only include values that evaluate false</li>
-  <li>ilimit - limit, or truncate, an iterator</li>
-  <li>irep - create a repeating iterator</li>
-  <li>izip - zip together multiple iterators</li>
-  <li>product - take the cartesian product of multiple iterators</li>
-  <li>recycle - recycle an iterator</li>
+  <li>chain: chain multiple iterators together into one iterator</li>
+  <li>enumerate: create an enumeration from an iterator</li>
+  <li>ihasNext: add a hasNext method to an iterator</li>
+  <li>ifilter: only return values for which a predicate function returns true</li>
+  <li>ifilterfalse: only return values for which a predicate function returns false</li>
+  <li>ilimit: limit, or truncate, an iterator</li>
+  <li>irep: create a repeating iterator</li>
+  <li>izip: zip together multiple iterators</li>
+  <li>product: zip together multiple iterators in cartesian product fashion</li>
+  <li>recycle: recycle values from an iterator repeatedly</li>
 </ul>
 </p>
 
